6f5f06359530edda397534d01c6c0902e2e39b1e
ken restivo <ken@restivo.org>
2011-10-18 21:23:35 -0700

    rudimenarly tables

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -3,22 +3,42 @@ CREATE TABLE t_student (
     full_name VARCHAR(128) NOT NULL,
     grade integer NOT NULL,
     emergency_1 VARCHAR(256) NOT NULL,
-    emergency_2 VARCHAR(256) NOT NULL,
-    emergency_2 VARCHAR(256) NOT NULL,
+    emergency_2 VARCHAR(256) ,
+    emergency_2 VARCHAR(256) 
 );
 
 CREATE TABLE t_teacher (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     full_name VARCHAR(128) NOT NULL,
-    email VARCHAR(256) NOT NULL,
-    phone VARCHAR(256) NOT NULL,
+    email VARCHAR(256),
+    phone VARCHAR(256)
 );
 
 
 CREATE TABLE t_class (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     class_name VARCHAR(128) NOT NULL,
-    description VARCHAR(256) NOT NULL,
-    grade_ranges VARCHAR(64) NOT NULL,
-    dates_times VARCHAR(256) NOT NULL,
+    description longtext,
+    min_grade_allowed int not null,
+    max_grade_allowed int not null,
+    dates_times VARCHAR(256),
+    cost decimal(19,2),
+    max_students int
 );
+
+CREATE TABLE t_check (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    amount decimal(19,2) not null,
+    payer VARCHAR(128) NOT NULL,
+    check_num int
+);
+
+
+CREATE TABLE t_deposit (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    amount decimal(19,2) not null,
+    deposit_date date not null
+);
+
+
+
