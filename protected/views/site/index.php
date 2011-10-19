dc92b473f26a3b66ab8db054818f4e481795a5f2
ken restivo <ken@restivo.org>
2011-10-18 22:36:24 -0700

    just test stuff

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,5 +1,11 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<p>This is a test page</p>
+<p>This is a test page <br />
+<a href="<?=Yii::app()->request->baseUrl?>?r=tStudent/admin">Students</a><br />
+<a href="<?=Yii::app()->request->baseUrl?>?r=tClass/admin">Classes</a><br />
+<a href="<?=Yii::app()->request->baseUrl?>?r=tTeacher/admin">Teachers</a><br />
+<a href="<?=Yii::app()->request->baseUrl?>?r=tCheck/admin">Checks</a><br />
+<a href="<?=Yii::app()->request->baseUrl?>?r=tDeposit/admin">Deposits</a><br />
+</p>
 
-<a href="<?=Yii::app()->request->baseUrl?>">It is here</a>
+
