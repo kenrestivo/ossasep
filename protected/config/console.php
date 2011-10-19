49bf562159b910bee97ccdce813c4c9ee2fde364
ken restivo <ken@restivo.org>
2011-10-18 21:26:57 -0700

    spellling dude

--- protected/config/console.php
+++ protected/config/console.php
@@ -15,7 +15,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=asep',
 			'emulatePrepare' => true,
 			'username' => 'asep',
-			'password' => 'student',
+			'password' => 'students',
 			'charset' => 'utf8',
 
 		),
