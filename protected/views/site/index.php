f24de8cb2906b443aa1e1a3133ab021ec77a5094
ken restivo <ken@restivo.org>
2011-11-05 12:00:47 -0700

    with links to the tables

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,4 +1,22 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<p>This is a test page</p>
+
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/CheckExpense">CheckExpense</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/CheckIncome">CheckIncome</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassInfo">ClassInfo</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassMeeting">ClassMeeting</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassSession">ClassSession</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ContactForm">ContactForm</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/DepositDetails">DepositDetails</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ExtraFee">ExtraFee</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Income">Income</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Instructor">Instructor</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/InstructorType">InstructorType</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/LoginForm">LoginForm</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/RequiredFor">RequiredFor</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/RequirementType">RequirementType</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Roster">Roster</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/SchoolCalendar">SchoolCalendar</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/SchoolYear">SchoolYear</a><br />
+ <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Student">Student</a><br />
 
