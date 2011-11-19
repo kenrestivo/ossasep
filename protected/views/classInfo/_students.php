a214311350ef9ef1fb68f6183c60b1fcd563fce9
ken restivo <ken@restivo.org>
2011-11-19 12:31:20 -0800

    well iit renders

--- protected/views/classInfo/_students.php
+++ protected/views/classInfo/_students.php
@@ -1,1 +1,30 @@
-foobarbaz
+<?php 
+echo CHTML::link("Add Student to Class", 
+                 array("Signup/create",
+                       'returnTo' => Yii::app()->request->requestUri));
+$this->widget('zii.widgets.grid.CGridView', array(
+                  'id'=>'signup-grid',
+                  'dataProvider'=>new KArrayDataProvider(
+                      $model->signups, 
+                      array('keyField' => 'student_id,class_id',
+                          )),
+                  'columns'=>array(
+                      array('name' => "Name",
+                            'value' => '$data->student->full_name'),
+                      array('name' => "Grade",
+                            'value' => '$data->student->grade'),
+                      /// TODO fix the rest of these
+                      'student.emergency_1',
+                      'student.emergency_2',
+                      'student.emergency_3',
+                      'student.parent_email',
+                      'signup',
+                      'status',
+                      array(
+                          'class'=>'CompositeButtonColumn',
+                          'modelClassName' => 'Signup',
+                          'returnTo' => Yii::app()->request->requestUri
+                          ),
+                      ),
+                  )); ?>
+
--- protected/views/classInfo/view.php
+++ protected/views/classInfo/view.php
@@ -24,16 +24,14 @@ $this->widget('CTabView',array(
         ),
         'tab2'=>array(
             'title'=>'Render tab',
-            'view' =>'_students'
+            'view' =>'_students',
+            'data' => array('model' => $model)
         ),
         'tab3'=>array(
             'title'=>'Url tab',
             'url'=>Yii::app()->createUrl("site/index"),
         )
     ),
-    'htmlOptions'=>array(
-        'style'=>'width:500px;'
-    )
 ));
 
 ?>
@@ -63,37 +61,6 @@ $this->widget('CTabView',array(
                         )
     ); 
 ?>
-
-<h3>Student signups </h3>
-<?php 
-echo CHTML::link("Add Student to Class", 
-                 array("Signup/create",
-                       'returnTo' => Yii::app()->request->requestUri));
-$this->widget('zii.widgets.grid.CGridView', array(
-                  'id'=>'signup-grid',
-                  'dataProvider'=>new KArrayDataProvider(
-                      $model->signups, 
-                      array('keyField' => 'student_id,class_id',
-                          )),
-                  'columns'=>array(
-                      array('name' => "Name",
-                            'value' => '$data->student->full_name'),
-                      array('name' => "Grade",
-                            'value' => '$data->student->grade'),
-                      /// TODO fix the rest of these
-                      'student.emergency_1',
-                      'student.emergency_2',
-                      'student.emergency_3',
-                      'student.parent_email',
-                      'signup',
-                      'status',
-                      array(
-                          'class'=>'CompositeButtonColumn',
-                          'modelClassName' => 'Signup',
-                          'returnTo' => Yii::app()->request->requestUri
-                          ),
-                      ),
-                  )); ?>
 
 
 
