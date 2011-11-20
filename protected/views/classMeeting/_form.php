d8bb87b725268f650b3ba09af9a99e69fb517c15
ken restivo <ken@restivo.org>
2011-11-19 20:47:00 -0800

    add datepicker

--- protected/views/classMeeting/_form.php
+++ protected/views/classMeeting/_form.php
@@ -11,7 +11,21 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'meeting_date'); ?>
-		<?php echo $form->textField($model,'meeting_date'); ?>
+<?php 
+$this->widget('zii.widgets.jui.CJuiDatePicker', array(
+  'model'=>$model,
+  'attribute'=>'meeting_date',
+  'value'=>$model->meeting_date,
+  // additional javascript options for the date picker plugin
+  'options'=>array(
+    'showAnim'=>'fold',
+    'showButtonPanel'=>true,
+    'autoSize'=>true,
+    'dateFormat'=>'yy-mm-dd',
+    'defaultDate'=>$model->meeting_date,
+   ),
+));
+?>
 		<?php echo $form->error($model,'meeting_date'); ?>
 	</div>
 
