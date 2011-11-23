f451093e992403cdefae851bd73ea527ecf18977
ken restivo <ken@restivo.org>
2011-11-23 00:30:57 -0800

    simple partial

--- protected/controllers/StudentController.php
+++ protected/controllers/StudentController.php
@@ -32,7 +32,7 @@ class StudentController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+                  'actions'=>array('index','view', 'import'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
@@ -178,4 +178,10 @@ class StudentController extends Controller
 			Yii::app()->end();
 		}
 	}
+
+	function actionImport()
+	{
+		$this->renderPartial('_roster_import');
+	}
+
 }
