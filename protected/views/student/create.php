1d475e12e3c1cd195e0d48262e9226e84ad16894
ken restivo <ken@restivo.org>
2011-11-23 01:20:42 -0800

    clearer. also helps to name things right

--- protected/views/student/create.php
+++ protected/views/student/create.php
@@ -3,30 +3,30 @@
 <script type="text/javascript">
       jQuery(
           function($) {
-              $('#importer').click(
-                  function (){
-                      $.ajax({
-                          url: "<?= $this->createUrl('import') ?>", 
-                                  dataType: "text", 
-                                  cache: false, 
-                                  success: function(data){
-                                  $("#test").html(data)
-                                      }}); 
-                      return(false);
-                  })})
+              function roster_import_box(){
+                  $.ajax({
+                      url: "<?= $this->createUrl('import') ?>", 
+                              dataType: "text", 
+                              cache: false, 
+                              success: function(data){
+                              $("#test").html(data)
+                                  }}); 
+                  return(false);
+              }
+              $('#importmenu').click(roster_import_box);
+          })
+      </script>
 
-</script>
-
-<?php
-$this->menu=array(
-    array('label'=>'Manage Students', 
-          'url'=>array('admin')),
-    array('label'=>'Import From Roster', 
-          'url'=> '#',
-          'linkOptions' =>array(
-              'id' => 'importmenu'
-              )
-        ));
+      <?php
+      $this->menu=array(
+          array('label'=>'Manage Students', 
+                'url'=>array('admin')),
+          array('label'=>'Import From Roster', 
+                'url'=> '#',
+                'linkOptions' =>array(
+                    'id' => 'importmenu'
+                    )
+              ));
 ?>
 
 <h1>Create Student</h1>
