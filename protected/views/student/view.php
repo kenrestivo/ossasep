ffe5a8be9a84c81621e1e525981a74e084df7921
ken restivo <ken@restivo.org>
2011-11-23 01:17:19 -0800

    readable

--- protected/views/student/view.php
+++ protected/views/student/view.php
@@ -8,7 +8,13 @@ $this->menu=array(
 	array('label'=>'List Student', 'url'=>array('index')),
 	array('label'=>'Create Student', 'url'=>array('create')),
 	array('label'=>'Update Student', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Student', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Delete Student', 
+          'url'=>'#', 
+          'linkOptions'=>array(
+              'submit'=>array(
+                  'delete',
+                  'id'=>$model->id),
+              'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Student', 'url'=>array('admin')),
 );
 ?>
