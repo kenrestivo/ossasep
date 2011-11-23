567615feacc718144f05bcb7c1643531101e5804
ken restivo <ken@restivo.org>
2011-11-23 00:17:58 -0800

    move it to a partial

---
+++ protected/views/student/_roster_import.php
@@ -0,0 +1,67 @@
+
+<p> Choose from roster, and click Import to fill in their info</p>
+
+
+    <?php 
+
+    $form=$this->beginWidget('CActiveForm', array(
+                                 'id'=>'rasta-import',
+                                 'enableAjaxValidation'=>false,
+                                 )); 
+
+echo 
+CHtml::listBox('rasta', null,
+               CHtml::listData(Roster::model()->findAll(), 
+                               'id', 'full_name'),
+               array('size' => 20)
+    );
+
+$this->endWidget();
+
+?>
+
+<?php Yii::app()->clientScript->registerCoreScript("jquery")?>
+
+<script type="text/javascript">
+	jQuery(function($) {
+
+            fieldmap = { 
+                'last_name' : 'last_name',
+                'first_name' : 'first_name',
+                'grade' : 'grade',
+                'phone' : 'emergency_1',
+                'cell_parent_1' : 'emergency_2',
+                'cell_parent_2' : 'emergency_3',
+                'email_parent_1' : 'parent_email',
+            };
+
+
+            function populate(data){
+                for(rfield in fieldmap){
+                    $('input#Student_' + fieldmap[rfield]).val(data[rfield]);
+                }
+                
+            }
+
+
+            get_rasta = function (num){
+                $.ajax({
+                    url: "<?php echo $this->createUrl('roster/json')?>/" + num,
+                            dataType: 'json',
+                            cache: false,
+                            success: function(data) {
+                            populate(data);
+}
+                    });
+            }
+            $('#importer').click(function(){
+                    get_rasta($("#rasta option:selected").val()); 
+                    return false
+                        })
+        });
+</script>
+
+
+<?php
+echo CHtml::link("Import", '#', array('id' => 'importer'));
+?>
