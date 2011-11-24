988557aad44c5164846327a3104d1c1b608a749b
ken restivo <ken@restivo.org>
2011-11-24 01:23:24 -0800

    put the student menu back in

--- protected/views/student/view.php
+++ protected/views/student/view.php
@@ -1,3 +1,14 @@
+<?php $this->menu=array(
+	array('label'=>'List Student', 'url'=>array('index')),
+	array('label'=>'Create Student', 'url'=>array('create')),
+	array('label'=>'Update Student', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete Student', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->tuid),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage Student', 'url'=>array('admin')),
+);
+
+?>
+
+
 <h1>View Student <?php echo $model->full_name; ?> </h1>
 
 
