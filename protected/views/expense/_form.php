683d9e1f5a493996ff197696373adbd62d44da7e
ken restivo <ken@restivo.org>
2011-11-24 16:26:13 -0800

    copy ove ra bunch of crap from signup for expense

---
+++ protected/views/expense/_form.php
@@ -0,0 +1,72 @@
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'signup-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'student_id'); ?>
+
+    <?php echo $form->dropDownList(
+        $model,'student_id',
+        CHtml::listData(Student::model()->findAll(), 'id', 'full_name')); ?>
+		<?php echo $form->error($model,'student_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'class_id'); ?>
+
+
+    <?php echo $form->dropDownList(
+        $model,'class_id',
+        CHtml::listData(ClassInfo::model()->findAll(), 'id', 'description')); ?>
+
+		<?php echo $form->error($model,'class_id'); ?>
+	</div>
+	<div class="row">
+		<?php echo $form->labelEx($model,'signup'); ?>
+
+<?php 
+$this->widget('zii.widgets.jui.CJuiDatePicker', array(
+  'model'=>$model,
+  'attribute'=>'signup',
+  'value'=>$model->signup,
+  // additional javascript options for the date picker plugin
+  'options'=>array(
+    'showAnim'=>'fold',
+    'showButtonPanel'=>true,
+    'autoSize'=>true,
+    'dateFormat'=>'yy-mm-dd',
+    'defaultDate'=>$model->signup,
+   ),
+));
+?>
+		<?php echo $form->error($model,'signup'); ?>
+	</div>
+
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'status'); ?>
+    <?php echo ZHtml::enumDropDownList( $model,'status'); ?>
+		<?php echo $form->error($model,'status'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'note'); ?>
+		<?php echo $form->textField($model,'note',array('size'=>60,'maxlength'=>256)); ?>
+		<?php echo $form->error($model,'note'); ?>
+	</div>
+
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
