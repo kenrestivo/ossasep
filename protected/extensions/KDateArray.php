6e40d3efbcd2bd00cf2b4fcb9f4b7d776353c399
ken restivo <ken@restivo.org>
2011-11-26 14:36:51 -0800

    ugly but  bit more readable

--- protected/extensions/KDateArray.php
+++ protected/extensions/KDateArray.php
@@ -14,9 +14,12 @@ class KDateArray {
         $aryRange=array();
 
         $iDateFrom=mktime(1,0,0,substr($strDateFrom,5,2),     
-                          substr($strDateFrom,8,2),substr($strDateFrom,0,4));
+                          substr($strDateFrom,8,2),
+                          substr($strDateFrom,0,4));
+
         $iDateTo=mktime(1,0,0,substr($strDateTo,5,2),     
-                        substr($strDateTo,8,2),substr($strDateTo,0,4));
+                        substr($strDateTo,8,2),
+                        substr($strDateTo,0,4));
 
         if ($iDateTo>=$iDateFrom) {
             array_push($aryRange,date('Y-m-d',$iDateFrom)); // first entry
