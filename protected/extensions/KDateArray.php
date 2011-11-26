f76c7bc41d31b4028d775dc50e35ef615b6276d6
ken restivo <ken@restivo.org>
2011-11-26 14:34:38 -0800

    format indent comment

--- protected/extensions/KDateArray.php
+++ protected/extensions/KDateArray.php
@@ -4,39 +4,38 @@ class KDateArray {
 
 // via http://boonedocks.net/mike/archives/137-Creating-a-Date-Range-Array-with-PHP.html
 
-public static function createDateRangeArray($strDateFrom,$strDateTo) {
+    public static function createDateRangeArray($strDateFrom,$strDateTo) {
 
 
-    // takes two dates formatted as YYYY-MM-DD and creates an
-    // inclusive array of the dates between the from and to dates.
+        // takes two dates formatted as YYYY-MM-DD and creates an
+        // inclusive array of the dates between the from and to dates.
 
-    // could test validity of dates here but I'm already doing
-    // that in the main script
+        // could test validity of dates here but I'm already doing
+        // that in the main script
 
-    $aryRange=array();
+        $aryRange=array();
 
-    $iDateFrom=mktime(1,0,0,substr($strDateFrom,5,2),     
-                      substr($strDateFrom,8,2),substr($strDateFrom,0,4));
-    $iDateTo=mktime(1,0,0,substr($strDateTo,5,2),     
-                    substr($strDateTo,8,2),substr($strDateTo,0,4));
+        $iDateFrom=mktime(1,0,0,substr($strDateFrom,5,2),     
+                          substr($strDateFrom,8,2),substr($strDateFrom,0,4));
+        $iDateTo=mktime(1,0,0,substr($strDateTo,5,2),     
+                        substr($strDateTo,8,2),substr($strDateTo,0,4));
 
-    if ($iDateTo>=$iDateFrom) {
-        array_push($aryRange,date('Y-m-d',$iDateFrom)); // first entry
+        if ($iDateTo>=$iDateFrom) {
+            array_push($aryRange,date('Y-m-d',$iDateFrom)); // first entry
 
-        while ($iDateFrom<$iDateTo) {
-            $iDateFrom+=86400; // add 24 hours
-            $wd=date("N", $iDateFrom);
-
-            echo "$wd";
-            if($wd > 0 && $wd < 6){
-                array_push($aryRange,date('Y-m-d',$iDateFrom));
+            while ($iDateFrom<$iDateTo) {
+                $iDateFrom+=86400; // add 24 hours
+                $wd=date("N", $iDateFrom);
+                // i only want the weekdays
+                if($wd > 0 && $wd < 6){
+                    array_push($aryRange,date('Y-m-d',$iDateFrom));
+                }
             }
         }
+        return $aryRange;
+
     }
-    return $aryRange;
-
-  }
 
 
 
-?>
+    ?>
