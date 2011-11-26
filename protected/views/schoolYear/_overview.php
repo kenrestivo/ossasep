6463d1687749455176539b424d8cccd6dda2ccd6
ken restivo <ken@restivo.org>
2011-11-26 15:11:19 -0800

    school year tabs

---
+++ protected/views/schoolYear/_overview.php
@@ -0,0 +1,30 @@
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'description',
+		'start_date',
+		'end_date',
+	),
+)); 
+
+
+
+echo CHTML::link("Edit  ". $model->description, 
+                 array('update', 'id'=>$model->id,
+                       'returnTo' => Yii::app()->request->requestUri));
+
+?> &nbsp;<?php
+echo CHTML::link("Delete  ". $model->description, 
+                 array('#', 
+                       'linkOptions'=>array(
+                           'submit'=>array(
+                               'delete',
+                               'id'=>$model->id),
+                           'confirm'=>'Are you sure you want to delete this item?')));
+
+
+
+
+?>
+
--- protected/views/schoolYear/view.php
+++ protected/views/schoolYear/view.php
@@ -1,26 +1,36 @@
 <?php
-$this->breadcrumbs=array(
-	'School Years'=>array('index'),
-	$model->id,
-);
-
 $this->menu=array(
 	array('label'=>'List SchoolYear', 'url'=>array('index')),
 	array('label'=>'Create SchoolYear', 'url'=>array('create')),
-	array('label'=>'Update SchoolYear', 'url'=>array('update', 'id'=>$model->id)),
 	array('label'=>'Delete SchoolYear', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage SchoolYear', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View SchoolYear #<?php echo $model->id; ?></h1>
+<h1>View SchoolYear <?php echo $model->description; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'description',
-		'start_date',
-		'end_date',
-	),
-)); ?>
+
+
+
+
+<?php
+
+
+$this->widget('zii.widgets.jui.CJuiTabs', 
+              array(
+                  'options'=>array(
+                      'cookie'=>array(
+                          'expires'=>30)),
+                  
+                  'tabs'=>array(
+                      'School Year' =>
+                      $this->renderPartial("_overview", 
+                                           array('model' => $model), true),
+                      )));
+
+
+
+?>
+
+
+
