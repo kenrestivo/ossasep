e9120ea59b799df21df53bbed38a69e1d3b88a61
ken restivo <ken@restivo.org>
2011-11-26 17:21:00 -0800

    sorta works

--- protected/views/schoolYear/_days.php
+++ protected/views/schoolYear/_days.php
@@ -1,7 +1,17 @@
 <?php 
+
+$c = Yii::app()->db->createCommand();
+$c->text = "select count(school_day) as count from school_calendar where school_year_id = 1;";
+$r=$c->queryRow();
+
+
+
+
 echo CHTML::link("Add Date to ". $model->description, 
                  array("SchoolCalendar/create",
                        'returnTo' => Yii::app()->request->requestUri));
+
+
 $this->widget('zii.widgets.grid.CGridView', array(
                   'id'=>'schoolcalendar-grid',
                   'dataProvider'=> new KArrayDataProvider($model->school_calendars),
