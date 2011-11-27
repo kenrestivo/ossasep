3cbb99d6559dcb3123b8325dfa4f7dc9c97d7ba8
ken restivo <ken@restivo.org>
2011-11-27 12:28:03 -0800

    works

--- protected/controllers/ClassInfoController.php
+++ protected/controllers/ClassInfoController.php
@@ -40,7 +40,7 @@ class ClassInfoController extends Controller
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+                  'actions'=>array('admin','delete', 'populate'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
