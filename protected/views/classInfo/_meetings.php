6868921669d456abe1da22e3f70fc8c3b7edcb81
ken restivo <ken@restivo.org>
2011-11-27 12:30:58 -0800

    that's it, the meeting days are in

--- protected/views/classInfo/_meetings.php
+++ protected/views/classInfo/_meetings.php
@@ -1,4 +1,18 @@
 <?php 
+
+$c = Yii::app()->db->createCommand("select count(meeting_date) as count from class_meeting where class_id = :classid;");
+$r=$c->queryRow(true, array('classid' => $model->id));
+
+
+if($r['count'] < 1 ){
+    echo CHTML::link("Auto-Populate Meeting Days for ". $model->class_name, 
+                     array('populate','id' => $model->id));
+    echo "<br />";
+}
+
+
+
+
 echo CHTML::link("Add Meeting Date for ". $model->class_name, 
 				 array("ClassMeeting/create",
 					   'returnTo' => Yii::app()->request->requestUri));
