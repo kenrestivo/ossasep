e46ef5f2ed822d08c8e0ec759c5dd229734f36ce
ken restivo <ken@restivo.org>
2011-11-28 13:32:01 -0800

    not really working, broken nesting

--- protected/views/checkIncome/entry_form.php
+++ protected/views/checkIncome/entry_form.php
@@ -7,19 +7,36 @@ return array(
         'amount'=>array(
             'type'=>'text',
             'maxlength'=>20,
-        ),
+            ),
         'check_num'=>array(
             'type'=>'text',
             'maxlength'=>10,
+            ),
+        'income'=>array(
+            'type'=>'form',
+            'title'=>'Apply Payment',
+            'elements'=>array(
+                'student_id' => array(
+                    'maxlength'=>10,
+                    'type'=> 'text',
+                    ),
+                'class_id' => array(
+                    'maxlength'=>10,
+                    'type'=> 'text',
+                    ),
+                'amount' => array(
+                    'type'=> 'text',
+                    'maxlength'=>10,
+                    ),
+                ),
+            ),
         ),
-    ),
- 
     'buttons'=>array(
         'entry_form'=>array(
             'type'=>'submit',
             'label'=>'Save', //TODO: or create
+            ),
         ),
-    ),
-);
+    );
 
 ?>
