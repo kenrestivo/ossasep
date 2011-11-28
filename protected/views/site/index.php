7066397e1f972c8e37d423a1590beabb93954012
ken restivo <ken@restivo.org>
2011-11-27 18:20:49 -0800

    cleaner layout

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,37 +1,63 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
+<div class="span-7">
+  <h1>Main views</h1>
 
-<h1>Main views</h1>
-
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassInfo">ClassInfo</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Student">Student</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Instructor">Instructor</a><br /> <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/CheckIncome">CheckIncome</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/CheckExpense">CheckExpense</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/DepositDetails">DepositDetails</a><br />
-<div><p></p></div>
-
-<h1>Setup</h1>
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/SchoolYear">SchoolYear</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassSession">ClassSession</a><br />
-<div><p></p></div>
-
-<h1>Utility Views</h1>
-      <p>(These may go away)</p>
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassMeeting">ClassMeeting</a><br />
-
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ExtraFee">ExtraFee</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Income">Income</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Expense">Expense</a><br />
-
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/InstructorType">InstructorType</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/InstructorAssignment">InstructorAssignment</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/RequiredFor">RequiredFor</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/RequirementType">RequirementType</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Roster">Roster</a><br />
-
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/SchoolCalendar">SchoolCalendar</a><br />
-
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Signup">Signup</a><br />
- <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/RequirementStatus">RequirementStatus</a><br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassInfo">ClassInfo</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Student">Student</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Instructor">Instructor</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/CheckIncome">CheckIncome</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/CheckExpense">CheckExpense</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/DepositDetails">DepositDetails</a>
+  <br />
+</div>
 
 
+<div class="span-7">
+  <h1>Setup</h1>
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/SchoolYear">SchoolYear</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassSession">ClassSession</a>
+  <br />
+</div>
+
+
+
+
+<div class="span-7">
+  <h1>Utility Views</h1>
+  <p>(These may go away)</p>
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ClassMeeting">ClassMeeting</a>
+  <br />
+
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/ExtraFee">ExtraFee</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Income">Income</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Expense">Expense</a>
+  <br />
+
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/InstructorType">InstructorType</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/InstructorAssignment">InstructorAssignment</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/RequiredFor">RequiredFor</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/RequirementType">RequirementType</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Roster">Roster</a>
+  <br />
+
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/SchoolCalendar">SchoolCalendar</a>
+  <br />
+
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Signup">Signup</a>
+  <br />
+  <a href="<?php echo Yii::app()->baseUrl; ?>/index.php/RequirementStatus">RequirementStatus</a>
+  <br />
+</div>
