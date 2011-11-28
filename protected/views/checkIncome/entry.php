10c6155e9f97b5e472f640b247527e74ab05cc61
ken restivo <ken@restivo.org>
2011-11-28 14:21:50 -0800

    js auto ennter

--- protected/views/checkIncome/entry.php
+++ protected/views/checkIncome/entry.php
@@ -1,5 +1,17 @@
 <h1>Check Entry</h1>
  
 <div class="form">
-<?php echo $form; ?>
+     <?php echo $form; ?>
 </div>
+
+<?php Yii::app()->clientScript->registerCoreScript("jquery")?>
+
+<script type="text/javascript">
+     jQuery(function($) {
+
+             $('input#CheckIncome_amount').change(function(){
+                     $('input#Income_amount').val($(this).val());
+                 })
+                 });
+
+</script>
