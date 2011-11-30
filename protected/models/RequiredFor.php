a98fef78914f000f62dfe583fc392ba141f46687
ken restivo <ken@restivo.org>
2011-11-29 20:07:52 -0800

    technically not necessary, but just to be complete

--- protected/models/RequiredFor.php
+++ protected/models/RequiredFor.php
@@ -55,7 +55,12 @@ class RequiredFor extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'instructor_type' => array(self::BELONGS_TO, 'InstructorType', 'instructor_type_id'),
+			'instructor_type' => array(self::BELONGS_TO, 
+                                       'InstructorType', 
+                                       'instructor_type_id'),
+			'requirement_type' => array(self::BELONGS_TO, 
+                                        'RequirementType', 
+                                        'requirement_type_id'),
 		);
 	}
 
