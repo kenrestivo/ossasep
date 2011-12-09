e5733a9145260b538cc5e8a6ee6db2735d6e07f3
ken restivo <ken@restivo.org>
2011-12-08 21:09:19 -0800

    moving crap around, breaking everything

--- index.php
+++ index.php
@@ -5,20 +5,14 @@ $yii='/usr/local/yii-framework/yii.php';
 $glob=dirname(__FILE__).'/protected/globals.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
-// important to override for production
-$prod=dirname(__FILE__).'/protected/production.php';
-if(is_readable($prod)){
-    // override yii loc for production
-    require_once($prod);
-}
-
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
                          
-                         require_once($yii);
-                         require_once($glob);
-                         Yii::createWebApplication($config)->run();
+                         
+require_once($yii);
+require_once($glob);
+Yii::createWebApplication($config)->run();
 
 
