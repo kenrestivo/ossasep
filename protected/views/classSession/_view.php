098c201eebffa85b2f031ba6060c0ff9ff4429c4
ken restivo <ken@restivo.org>
2011-12-12 00:07:37 -0800

    slightly more usabel class sessino update

--- protected/views/classSession/_view.php
+++ protected/views/classSession/_view.php
@@ -1,11 +1,13 @@
 <div class="view">
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('school_year_id')); ?>:</b>
-	<?php echo CHtml::encode($data->school_year_id); ?>
+	<?php echo CHtml::encode($data->school_year->description); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->description)); ?>
+	<?php echo CHtml::link(CHtml::encode($data->description), 
+                           array('view', 'id' => $data->id)
+        ); ?>
 	<br />
 
  	<b><?php echo CHtml::encode($data->getAttributeLabel('start_date')); ?>:</b>
--- protected/views/classSession/update.php
+++ protected/views/classSession/update.php
@@ -13,6 +13,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Update ClassSession <?php echo $model->id; ?></h1>
+<h1>Update ClassSession <?php echo $model->summary; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
