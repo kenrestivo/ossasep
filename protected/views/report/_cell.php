0273780a95b19a193f40bfa08018027f4004255d
ken restivo <ken@restivo.org>
2011-12-12 20:06:01 -0800

    i have instructors, i am done-ish

--- protected/views/report/_cell.php
+++ protected/views/report/_cell.php
@@ -1,4 +1,16 @@
 <strong><?php echo CHtml::encode($model->class_name); ?></strong><br />
+
+
+<?php
+echo implode(CHtml::encode(' & '), 
+        array_map(
+            function($i) { return $i->full_name ; },
+            $model->instructors
+            ));
+
+?>
+<br />
+
 <?php echo CHtml::encode($model->min_grade_allowed); ?> - <?php echo CHtml::encode($model->max_grade_allowed); ?> <br />
 <?php echo CHtml::encode($model->start_time); ?> - <?php echo CHtml::encode($model->end_time); ?> <br />
 <?php echo CHtml::encode($model->location); ?>
