a81d8f42b6b0a39b2834f4976ede5cf0e1fb7052
ken restivo <ken@restivo.org>
2011-12-12 19:53:47 -0800

    it works, formatted stuff!

--- protected/views/report/_cell.php
+++ protected/views/report/_cell.php
@@ -1,1 +1,4 @@
-<?php echo $model->class_name; ?>
+<strong><?php echo CHtml::encode($model->class_name); ?></strong><br />
+<?php echo CHtml::encode($model->min_grade_allowed); ?> - <?php echo CHtml::encode($model->max_grade_allowed); ?> <br />
+<?php echo CHtml::encode($model->start_time); ?> - <?php echo CHtml::encode($model->end_time); ?> <br />
+<?php echo CHtml::encode($model->location); ?>
