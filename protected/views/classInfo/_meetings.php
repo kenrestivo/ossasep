ce9aeda4c2279a93880c18cae443a12101e3a206
ken restivo <ken@restivo.org>
2011-12-13 16:34:56 -0800

    show the class start/end times

--- protected/views/classInfo/_meetings.php
+++ protected/views/classInfo/_meetings.php
@@ -5,19 +5,23 @@ $am=$model->active_meetings;
 <p><?= count($am) ?> meetings, <?= count($model->meetings) - count($am) ?> makeup day </p>
 
 
-<?
+      <?
 if(count($am) < 1 ){
-
+    echo '<div><span class="span-8">';
     echo CHtml::beginForm(array('populate', 'id' => $model->id));
-    echo "Auto-add dates: ";
+    echo "Auto-populate meeting dates: ";
     echo CHtml::textField('num', 8, array('size' => 2));
     echo CHtml::submitButton('Add');
     echo CHtml::endForm();
 
-
-    echo "<br />";
+    echo "</span><span class=\"span-8 last\">Your session settings are start: " . $model->session->start_date. ", end " . $model->session->end_date . "</span>";
+    echo "</div><div><br />";
 }
+?>
 
+<div style="clear:both;">
+
+<?php
 echo CHTML::link("Add 1 Meeting Date for ". $model->class_name, 
 				 array("ClassMeeting/create",
                        'class_id' => $model->id,
@@ -31,8 +35,8 @@ $this->widget('zii.widgets.grid.CGridView', array(
                       'meeting_date',
                       'note',
                       'makeup',
-        'school_day.minimum',
-        'school_day.day_off',
+                      'school_day.minimum',
+                      'school_day.day_off',
                       array(
                           'class'=>'CompositeButtonColumn',
                           'modelClassName' => 'ClassMeeting',
@@ -42,3 +46,4 @@ $this->widget('zii.widgets.grid.CGridView', array(
 				  )); ?>
 
 
+</div>
