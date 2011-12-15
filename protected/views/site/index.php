4c50c907d8a0667509b812720a071f9527929383
ken restivo <ken@restivo.org>
2011-12-14 22:12:10 -0800

    better note to heidi

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,8 +3,10 @@ $this->breadcrumbs = array() ;
 // reset 
 ?>
 <h1>Welcome to Ocean Shore ASEP</h2>
-
+<p>
 <?= CHtml::link("Check our Schedule", 
                  array("/report/weekday")); ?>
-
-
+</p>
+<p>
+(Note: you will have to log in to see all the admin stuff)
+</p>
