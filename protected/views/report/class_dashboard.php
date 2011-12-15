fe93eb82845994d153e53ccf289d0c57d3b9c49c
ken restivo <ken@restivo.org>
2011-12-14 22:05:13 -0800

    links for the dashboard

--- protected/views/report/class_dashboard.php
+++ protected/views/report/class_dashboard.php
@@ -4,7 +4,13 @@ $this->widget('zii.widgets.grid.CGridView', array(
                   'id'=>'classdashsboard-grid',
                   'dataProvider'=> $classes,
                   'columns'=>array(
-                      'class_name:text:Class',
+                      array(
+                          'class'=>'CLinkColumn',
+                          'labelExpression'=>'$data["class_name"]',
+                          'urlExpression'=>
+                          'Yii::app()->createUrl("/ClassInfo/view",array("id"=>$data["id"]))',
+                          'header'=>'Class'
+                          ),
                       'signup_status:text:Status',
                       'signups:nozero:Signed up',
                       'waitlist:nozero:Waitlisted',
