04803d7f528ba523818c107e9729f25efaf6f6e3
ken restivo <ken@restivo.org>
2011-12-21 15:53:38 -0800

    dev server sandbox warning

--- protected/config/dev.php
+++ protected/config/dev.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Ocean Shore After School Enrichment Program',
+	'name'=>'OSSASEP SANDBOX (DEVELOPMENT SERVER)',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
