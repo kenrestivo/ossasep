8b80ad981e440bf63346d67d7f2f9961de046583
ken restivo <ken@restivo.org>
2011-12-21 01:26:41 -0800

    not short name

--- protected/views/checkIncome/view.php
+++ protected/views/checkIncome/view.php
@@ -13,7 +13,7 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Check <?php echo $model->short_name; ?></h1>
+<h1>View Check <?php echo $model->summary; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
