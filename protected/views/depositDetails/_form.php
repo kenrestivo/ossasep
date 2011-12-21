8f583382b8b511d84253f326cbaa2b91fab3a21f
ken restivo <ken@restivo.org>
2011-12-21 13:55:40 -0800

    reorder

--- protected/views/depositDetails/_form.php
+++ protected/views/depositDetails/_form.php
@@ -21,58 +21,11 @@
 		<?php echo $form->error($model,'total_amount'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'pennies'); ?>
-		<?php echo $form->textField($model,'pennies'); ?>
-		<?php echo $form->error($model,'pennies'); ?>
-	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'nickels'); ?>
-		<?php echo $form->textField($model,'nickels'); ?>
-		<?php echo $form->error($model,'nickels'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'dimes'); ?>
-		<?php echo $form->textField($model,'dimes'); ?>
-		<?php echo $form->error($model,'dimes'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'quarters'); ?>
-		<?php echo $form->textField($model,'quarters'); ?>
-		<?php echo $form->error($model,'quarters'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'dollar_coins'); ?>
-		<?php echo $form->textField($model,'dollar_coins'); ?>
-		<?php echo $form->error($model,'dollar_coins'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'ones'); ?>
-		<?php echo $form->textField($model,'ones'); ?>
-		<?php echo $form->error($model,'ones'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'fives'); ?>
-		<?php echo $form->textField($model,'fives'); ?>
-		<?php echo $form->error($model,'fives'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'tens'); ?>
-		<?php echo $form->textField($model,'tens'); ?>
-		<?php echo $form->error($model,'tens'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'twenties'); ?>
-		<?php echo $form->textField($model,'twenties'); ?>
-		<?php echo $form->error($model,'twenties'); ?>
+		<?php echo $form->labelEx($model,'hundreds'); ?>
+		<?php echo $form->textField($model,'hundreds'); ?>
+		<?php echo $form->error($model,'hundreds'); ?>
 	</div>
 
 	<div class="row">
@@ -82,10 +35,65 @@
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'hundreds'); ?>
-		<?php echo $form->textField($model,'hundreds'); ?>
-		<?php echo $form->error($model,'hundreds'); ?>
+		<?php echo $form->labelEx($model,'twenties'); ?>
+		<?php echo $form->textField($model,'twenties'); ?>
+		<?php echo $form->error($model,'twenties'); ?>
 	</div>
+
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'tens'); ?>
+		<?php echo $form->textField($model,'tens'); ?>
+		<?php echo $form->error($model,'tens'); ?>
+	</div>
+
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'fives'); ?>
+		<?php echo $form->textField($model,'fives'); ?>
+		<?php echo $form->error($model,'fives'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'ones'); ?>
+		<?php echo $form->textField($model,'ones'); ?>
+		<?php echo $form->error($model,'ones'); ?>
+	</div>
+
+
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'dollar_coins'); ?>
+		<?php echo $form->textField($model,'dollar_coins'); ?>
+		<?php echo $form->error($model,'dollar_coins'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'quarters'); ?>
+		<?php echo $form->textField($model,'quarters'); ?>
+		<?php echo $form->error($model,'quarters'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'dimes'); ?>
+		<?php echo $form->textField($model,'dimes'); ?>
+		<?php echo $form->error($model,'dimes'); ?>
+	</div>
+	<div class="row">
+		<?php echo $form->labelEx($model,'nickels'); ?>
+		<?php echo $form->textField($model,'nickels'); ?>
+		<?php echo $form->error($model,'nickels'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'pennies'); ?>
+		<?php echo $form->textField($model,'pennies'); ?>
+		<?php echo $form->error($model,'pennies'); ?>
+	</div>
+
+
+
+
 	<div class="row">
 		<?php echo $form->labelEx($model,'note'); ?>
 		<?php echo $form->textField($model,'note',array('size'=>60,'maxlength'=>256)); ?>
