434f9ee6abbfba1854d1fb1ffda6ddb39f7ddbb2
ken restivo <ken@restivo.org>
2011-12-20 22:24:52 -0800

    form

---
+++ protected/views/checkIncome/_multi_entry.php
@@ -0,0 +1,123 @@
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'check-income-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'amount'); ?>
+		<?php echo $form->textField($model,'amount',array('size'=>19,'maxlength'=>19)); ?>
+		<?php echo $form->error($model,'amount'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'payer'); ?>
+		<?php echo $form->textField($model,'payer',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'payer'); ?>
+	</div>
+
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'payee_id'); ?>
+    <?php echo $form->dropDownList(
+        $model,'payee_id',
+        CHtml::listData(Company::model()->findAll(), 'id', 'name')); ?>
+		<?php echo $form->error($model,'payee_id'); ?>
+
+		<?php echo $form->error($model,'payee_id'); ?>
+	</div>
+
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'check_num'); ?>
+		<?php echo $form->textField($model,'check_num'); ?>
+		<?php echo $form->error($model,'check_num'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'check_date'); ?>
+<?php 
+$this->widget('zii.widgets.jui.CJuiDatePicker', array(
+  'model'=>$model,
+  'attribute'=>'check_date',
+  'value'=>$model->check_date,
+  // additional javascript options for the date picker plugin
+  'options'=>array(
+    'showAnim'=>'fold',
+    'showButtonPanel'=>true,
+    'autoSize'=>true,
+    'dateFormat'=>'yy-mm-dd',
+    'defaultDate'=>$model->check_date,
+   ),
+));
+?>
+		<?php echo $form->error($model,'check_date'); ?>
+	</div>
+
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'deposit_id'); ?>
+<?php echo $form->dropDownList(
+    $model,'deposit_id',
+    CHtml::listData(DepositDetails::model()->findAll(), 
+                    'id', 'deposited_date')); ?>
+		<?php echo $form->error($model,'deposit_id'); ?>
+	</div>
+
+    <p>TODO: add javascript to only show deposit if company is PTO, and only show delviered if company is NOT pto </p>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'delivered'); ?>
+
+<?php 
+$this->widget('zii.widgets.jui.CJuiDatePicker', array(
+  'model'=>$model,
+  'attribute'=>'delivered',
+  'value'=>$model->delivered,
+  // additional javascript options for the date picker plugin
+  'options'=>array(
+    'showAnim'=>'fold',
+    'showButtonPanel'=>true,
+    'autoSize'=>true,
+    'dateFormat'=>'yy-mm-dd',
+    'defaultDate'=>$model->delivered,
+   ),
+));
+?>
+		<?php echo $form->error($model,'delivered'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'returned'); ?>
+
+<?php 
+$this->widget('zii.widgets.jui.CJuiDatePicker', array(
+  'model'=>$model,
+  'attribute'=>'returned',
+  'value'=>$model->returned,
+  // additional javascript options for the date picker plugin
+  'options'=>array(
+    'showAnim'=>'fold',
+    'showButtonPanel'=>true,
+    'autoSize'=>true,
+    'dateFormat'=>'yy-mm-dd',
+    'defaultDate'=>$model->returned,
+   ),
+));
+?>
+		<?php echo $form->error($model,'returned'); ?>
+	</div>
+
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
