c15f49d098f39dcfb599b3115a45273cb8913c5a
ken restivo <ken@restivo.org>
2011-12-23 19:54:17 -0800

    add single-ends for instructor assignemnts

--- protected/views/instructorAssignment/_form.php
+++ protected/views/instructorAssignment/_form.php
@@ -1,5 +1,7 @@
 <div class="form">
 
+<?php $this->widget( 'ext.EChosen.EChosen'); ?>
+
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'instructorassignment-form',
 	'enableAjaxValidation'=>false,
@@ -9,14 +11,33 @@
 
 	<?php echo $form->errorSummary($model); ?>
 
+
+<?php
+if(!isset($_GET['instructor_id'])){
+?>
+
 	<div class="row">
 		<?php echo $form->labelEx($model,'instructor_id'); ?>
 
     <?php echo $form->dropDownList(
         $model,'instructor_id',
-        CHtml::listData(Instructor::model()->findAll(), 'id', 'full_name')); ?>
+        CHtml::listData(Instructor::model()->findAll(), 'id', 'full_name'),
+        array('class' => 'chzn-select')); ?>
 		<?php echo $form->error($model,'instructor_id'); ?>
 	</div>
+
+    <?php } else { ?>
+    Add class for: 
+	<?php echo CHtml::encode($model->instructor->full_name);
+          echo $form->hiddenField($model,"instructor_id"); 
+               } ?>
+
+
+
+
+<?php
+if(!isset($_GET['class_id'])){
+?>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'class_id'); ?>
@@ -24,10 +45,16 @@
 
     <?php echo $form->dropDownList(
         $model,'class_id',
-        CHtml::listData(ClassInfo::model()->findAll(), 'id', 'summary')); ?>
+        CHtml::listData(ClassInfo::model()->findAll(), 'id', 'summary'),
+         array('class' => 'chzn-select')); ?>
 
 		<?php echo $form->error($model,'class_id'); ?>
 	</div>
+    <?php } else { ?>
+    Add instructor for: 
+	<?php echo CHtml::encode($model->class->summary);
+          echo $form->hiddenField($model,"class_id"); 
+               } ?>
 
 
 	<div class="row">
