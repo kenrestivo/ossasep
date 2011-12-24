9d20ef2ca5de5f347f3f1941d9bfe6a01efad8a4
ken restivo <ken@restivo.org>
2011-12-23 19:44:20 -0800

    use echosen AND show single-legged linktables (ugly)

--- protected/views/signup/_form.php
+++ protected/views/signup/_form.php
@@ -1,5 +1,7 @@
 <div class="form">
 
+<?php $this->widget( 'ext.EChosen.EChosen'); ?>
+
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'signup-form',
 	'enableAjaxValidation'=>false,
@@ -9,14 +11,25 @@
 
 	<?php echo $form->errorSummary($model); ?>
 
+
+<?php
+if(!isset($_GET['student_id'])){
+?>
 	<div class="row">
 		<?php echo $form->labelEx($model,'student_id'); ?>
 
     <?php echo $form->dropDownList(
         $model,'student_id',
-        CHtml::listData(Student::model()->findAll(), 'id', 'full_name')); ?>
+        CHtml::listData(Student::model()->findAll(), 'id', 'full_name'),
+        array('class' => 'chzn-select')); ?>
 		<?php echo $form->error($model,'student_id'); ?>
 	</div>
+    <?php } else { ?>
+    Add class for: 
+	<?php echo CHtml::encode($model->student->full_name);
+          echo $form->hiddenField($model,"student_id"); 
+               } ?>
+
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'class_id'); ?>
@@ -24,7 +37,8 @@
 
     <?php echo $form->dropDownList(
         $model,'class_id',
-        CHtml::listData(ClassInfo::model()->findAll(), 'id', 'summary')); ?>
+        CHtml::listData(ClassInfo::model()->findAll(), 'id', 'summary'), 
+        array('class' => 'chzn-select')); ?>
 
 		<?php echo $form->error($model,'class_id'); ?>
 	</div>
