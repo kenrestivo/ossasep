0142954823a3a8741b359927b005d444e231d2c3
ken restivo <ken@restivo.org>
2011-12-25 01:19:02 -0800

    cleaner single-end

--- protected/views/classMeeting/_form.php
+++ protected/views/classMeeting/_form.php
@@ -35,13 +35,23 @@ $this->widget('zii.widgets.jui.CJuiDatePicker', array(
 		<?php echo $form->error($model,'note'); ?>
 	</div>
 
+
+
 	<div class="row">
 		<?php echo $form->labelEx($model,'class_id'); ?>
 
-<?php echo $form->dropDownList(
-    $model,'class_id',
-    CHtml::listData(ClassInfo::model()->findAll(), 
-                    'id', 'summary')); ?>
+<?php 
+if(!isset($_GET['class_id'])){
+    echo $form->dropDownList(
+        $model,'class_id',
+        CHtml::listData(ClassInfo::model()->findAll(), 
+                        'id', 'summary')); 
+} else {
+	echo CHtml::encode($model->class->summary);
+    echo $form->hiddenField($model,"class_id"); 
+} 
+
+?>
 
 		<?php echo $form->error($model,'class_id'); ?>
 	</div>
