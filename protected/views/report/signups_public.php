70a08a3a54cd144369d8a8baa796c2531384582c
ken restivo <ken@restivo.org>
2011-12-24 21:45:24 -0800

    well it helps a little

--- protected/views/report/signups_public.php
+++ protected/views/report/signups_public.php
@@ -1,4 +1,26 @@
 <h1>Signup Sheet</h1>
+
+<?php 
+Yii::app()->clientScript->registerCoreScript('jquery');
+Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl.'/js/jquery.masonry.min.js');
+?>
+
+
+<script type="text/javascript">
+
+$(function(){
+  $('#masonry').masonry({
+    // options
+    itemSelector : '.emailable',
+    columnWidth : 300
+  });
+});
+
+</script>
+
+
+
+<div id="masonry">
 <?php
 foreach($classes as $class){
     $this->renderPartial(
@@ -7,3 +29,4 @@ foreach($classes as $class){
 }
 
 ?>
+</div>
