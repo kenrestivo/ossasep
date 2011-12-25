4aa37a6894ba5f139dc8b6f472895000f0cf2978
ken restivo <ken@restivo.org>
2011-12-24 20:20:24 -0800

    beginning of emailable

--- protected/views/report/_signup_public.php
+++ protected/views/report/_signup_public.php
@@ -1,13 +1,31 @@
-<h3><?= CHtml::link($model->class_name, 
-                     array("/ClassInfo/view", 'id' => $model->id)) ?>
-     <?= $model->gradeSummary('short') ?> <?= CHtml::encode($model->instructorNames(' and ')) ?></h3>
+<table class="bordertable">
+  <tr>
+    <th colspan="3" >
+      <?= CHtml::link($model->class_name, 
+        array("/ClassInfo/view", 'id' => $model->id)) ?>
+      <span style="float:right"><?= $model->gradeSummary('short') ?></span>
+    </th>
+  </tr>
+  <tr>
+    <td colspan="3">
+      <?= CHtml::encode($model->instructorNames(' and ')) ?>
+    </td>
+  </tr>
+  <tr>
+    <td>num</td>
+    <td>student</td>
+    <td>grade</td>
+  </tr>
+</table>
 
 <?php
-     
+  
+
+
 $kad = new KArrayDataProvider(
-    $model->sortedSignups,
-    array('keyField' => 'student_id,class_id',
-        ));
+$model->sortedSignups,
+array('keyField' => 'student_id,class_id',
+));
 
 ?>
 
