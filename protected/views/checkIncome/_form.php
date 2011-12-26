b8b13da3b07373cd410e1ef3cbea2947cf9a72d9
ken restivo <ken@restivo.org>
2011-12-25 18:34:13 -0800

    rename the views

--- protected/views/checkIncome/_form.php
+++ protected/views/checkIncome/_form.php
@@ -1,4 +1,4 @@
-<div class="form">
+<div class="wide form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'check-income-form',
@@ -8,6 +8,18 @@
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'check_num'); ?>
+		<?php echo $form->textField($model,'check_num'); ?>
+		<?php echo $form->error($model,'check_num'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'cash'); ?>
+		<?php echo $form->checkbox($model,'cash'); ?>
+		<?php echo $form->error($model,'cash'); ?>
+	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'amount'); ?>
@@ -43,18 +55,8 @@ $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
 	</div>
 
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'cash'); ?>
-		<?php echo $form->checkbox($model,'cash'); ?>
-		<?php echo $form->error($model,'cash'); ?>
-	</div>
 
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'check_num'); ?>
-		<?php echo $form->textField($model,'check_num'); ?>
-		<?php echo $form->error($model,'check_num'); ?>
-	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'check_date'); ?>
--- protected/views/checkIncome/update.php
+++ protected/views/checkIncome/update.php
@@ -13,6 +13,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Update CheckIncome <?php echo $model->id; ?></h1>
+<h1>Update Check <?php echo $model->summary; ?> (received)</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
