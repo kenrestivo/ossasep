116f791a3c01269b669fb9a1715fcaa502f11c7f
ken restivo <ken@restivo.org>
2011-12-28 01:17:47 -0800

    ugly but it works

--- protected/views/admin/osspto_instructors.php
+++ protected/views/admin/osspto_instructors.php
@@ -1,23 +1,29 @@
 <h1>OSSPTO-paid Instructors</h1>
 
 <table class="bordertable">
+<tr><th>Instructor</th><th>Classes</th><th>Received</th><th>Paid</th><th>Delivered</th></tr>
 <?php
 foreach($instructors as $i){
     echo "<tr>";
     echo "<td>" . $i->full_name . '</td>';
     echo "<td>";
     $owed = 0.0;
+    echo '<table class="catalog">';
     foreach($i->instructor_assignments as $c){
+    echo "<tr>";
         $s = $c->split($c->class->paid);
         $owed += $s;
-        echo $c->class->class_name . ' ' . $c->percentage . ' ' . $s;
-        echo '<br />';
+        echo '<td>'.CHtml::encode($c->class->summary) . '</td>';
+        echo '<td>'. Yii::app()->format->percent($c->percentage) . '</td>';
+        echo '<td>'.Yii::app()->format->currency($s) . '</td>';
+        echo "</tr>";
     }
-
+    echo '<td>Total</td><td></td><td>' . Yii::app()->format->currency($owed).'</td>';
+    echo '</table>';
     echo '</td>';
-    echo "<td>$owed</td>";
-    echo '<td>' .  $i->paid .'</td>';
-    echo '<td>' .  $i->delivered .'</td>';
+    echo '<td>'.Yii::app()->format->currency($owed).'</td>';
+    echo '<td>' .  Yii::app()->format->currency($i->paid) .'</td>';
+    echo '<td>' .  Yii::app()->format->currency($i->delivered) .'</td>';
     echo "</tr>";
 }
 ?>
