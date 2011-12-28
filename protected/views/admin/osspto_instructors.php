fda2e44c8708c4dfce4112c1258838263daec05b
ken restivo <ken@restivo.org>
2011-12-28 01:03:12 -0800

    show the totals

--- protected/views/admin/osspto_instructors.php
+++ protected/views/admin/osspto_instructors.php
@@ -7,7 +7,8 @@ foreach($instructors as $i){
     echo "<td>" . $i->full_name . '</td>';
     echo "<td>";
     foreach($i->instructor_assignments as $c){
-        echo $c->class->class_name . ' ' . $c->percentage;
+        echo $c->class->class_name . ' ' . $c->percentage . ' ';
+        echo $c->split($c->class->paid);
         echo '<br />';
     }
 
