bb4aa4c167c41d9b929faba3406b0a4f663a82b3
ken restivo <ken@restivo.org>
2011-12-27 19:28:36 -0800

    a way of gettning signups linked to income

--- protected/models/Income.php
+++ protected/models/Income.php
@@ -79,6 +79,19 @@ class Income extends CActiveRecord
             );
 	}
 
+
+    /*
+      Because activerecord sucks.
+     */
+    public function getSignup()
+    {
+        return Signup::model()->find(
+            array("condition" => "(class_id = :cid and student_id = :sid)",
+                  'params' => array(
+                      'cid' => $this->class_id,
+                      'sid' => $this->student_id)));
+    }
+
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
