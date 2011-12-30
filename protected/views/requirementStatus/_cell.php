f3be8af62b0c4a1a64a0ed3baef1f78761cc5ddb
ken restivo <ken@restivo.org>
2011-12-29 20:52:22 -0800

    ooh, pretty colors!

--- protected/views/requirementStatus/_cell.php
+++ protected/views/requirementStatus/_cell.php
@@ -2,16 +2,16 @@
 $res="";
 $class="";
 if(!isset($model) || $model->is_missing){
-    $class = "error";
+    $class = "status-error";
     $res = "Missing";
 } else if($model->is_expiring)  {
-    $class = "warning";
+    $class = "status-warning";
     $res = 'Expiring ' . Yii::app()->format->date($model->expired);
 } else if($model->is_expired)  {
-    $class = "error";
+    $class = "status-error";
     $res = "Expired " . Yii::app()->format->date($model->expired);;
 } else {
-    $class = "ok";
+    $class = "status-ok";
     if(isset($model->expired)){
         $res = CHtml::encode("OK - " . Yii::app()->format->date($model->expired));
     } else {
