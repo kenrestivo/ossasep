a2c220e476c0f649722fb06dcd299d569e7f419f
ken restivo <ken@restivo.org>
2011-12-29 20:58:55 -0800

    link optinoal

--- protected/views/admin/instructor_paperwork.php
+++ protected/views/admin/instructor_paperwork.php
@@ -2,7 +2,14 @@
 
 <?php
 foreach($instructors as $i){
-    echo '<span class="span-4"><b>' . CHtml::encode($i->full_name) . '</b></span>';
+    echo '<span class="span-4"><b>' ;
+
+    if(Yii::app()->user->name != 'admin' ){
+        echo CHtml::encode($i->full_name);
+    } else {
+        echo CHtml::link(CHtml::encode($i->full_name), array('/Instructor/view', 'id'=>$i->id)); 
+    }
+    echo '</b></span>';
     echo '<span class="span-16 last">';
     $this->renderPartial(
         '/instructor/_requirement_status',
