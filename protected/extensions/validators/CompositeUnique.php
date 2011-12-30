9068a489b6456461277b344470f560149f0361d5
ken restivo <ken@restivo.org>
2011-12-30 13:20:31 -0800

    repair the stupid bugs, and now ajax validation works generically

--- protected/extensions/validators/CompositeUnique.php
+++ protected/extensions/validators/CompositeUnique.php
@@ -1,22 +1,25 @@
+<?php
+
 class CompositeUnique extends CValidator
 {
-    
+    public $error_message = 'echo "Duplicate, Entry already exists"';
+
     protected function validateAttribute($object,$attribute)
     {
 
         $found = Signup::model()->findByPk($object->primaryKey);
         if($found){
-            $err = isset($params['error_message']) ? 
-                $object->evaluateExpression($params['error_message'], 
-                                          array('data' => $found)): 
-                'Duplicate, Entry already exists';
-            if(is_array($object->primaryKey)){
-                foreach($object->primaryKey as $k => $v){
-                    $object->addError($object, $k, $err);
+            $err= $object->evaluateExpression(
+                $this->error_message, 
+                array('data' => $found));
+
+            if(is_array($found->primaryKey)){
+                foreach($found->primaryKey as $k => $v){
+                    $this->addError($object, $k, $err);
                 }
             } else {
                 //XXX this is realy dead code, will i use this with non-composite?
-                $object->addError($object, $attribute, $err);
+                $this->addError($object, $attribute, $err);
             }
         }
 
