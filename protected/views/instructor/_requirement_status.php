204c3b7b71aef6ef39fe92e14afadd299ad90502
ken restivo <ken@restivo.org>
2011-12-29 20:01:56 -0800

    move the stuff to the cell, does NOT work here for some reason

--- protected/views/instructor/_requirement_status.php
+++ protected/views/instructor/_requirement_status.php
@@ -15,7 +15,6 @@ foreach($reqs as $r){
 echo '</tr><tr>';
 
 foreach($reqs as $r){ 
-    echo '<td>';
     $s=$this->subById($rs,  
                       array('instructor_id' => $model->id,
                             'requirement_type_id' => $r->id
@@ -25,7 +24,6 @@ foreach($reqs as $r){
                              array('model' => $s));
 
     }
-    echo '</td>';
 }
 ?>
 </tr>
--- protected/views/requirementStatus/_cell.php
+++ protected/views/requirementStatus/_cell.php
@@ -1,4 +1,21 @@
 <?php
-        echo Yii::app()->format->date($model->expired);
-
+$res="";
+$class="";
+if($model->missing){
+    $class = "error";
+    $res = "Missing";
+} else if($model->expiring)  {
+    $class = "warning";
+    $res = 'Expiring ' . Yii::app()->format->date($model->expired);
+} else if($model->expired)  {
+    $class = "error";
+    $res = "Expired " . Yii::app()->format->date($model->expired);;
+} else {
+    $res = "OK";
+}
+$res .= CHtml::encode($model->note);
 ?>
+
+<td class="<?= $class ?>">
+<?= $res ?>
+</td>
