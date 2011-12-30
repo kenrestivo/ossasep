7e5ce18c46619e2eec3f0bbad204fe950207a4bb
ken restivo <ken@restivo.org>
2011-12-29 20:47:06 -0800

    i'm gonna call this good enough

--- protected/views/admin/instructor_paperwork.php
+++ protected/views/admin/instructor_paperwork.php
@@ -2,11 +2,12 @@
 
 <?php
 foreach($instructors as $i){
-    echo '<h3>' . CHtml::encode($i->full_name) . '</h3>';
-
+    echo '<span class="span-4"><b>' . CHtml::encode($i->full_name) . '</b></span>';
+    echo '<span class="span-16 last">';
     $this->renderPartial(
         '/instructor/_requirement_status',
         array('model' =>$i));
+    echo '</span><div class="clear"></div>';
 }
 
 ?>
