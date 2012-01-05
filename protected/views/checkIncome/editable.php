fdd9c7e6810b33e50238c0c0dfd8967227c0aecb
ken restivo <ken@restivo.org>
2012-01-05 14:54:13 -0800

    add view, make validator happy, but break saving!

--- protected/views/checkIncome/editable.php
+++ protected/views/checkIncome/editable.php
@@ -11,18 +11,22 @@ echo CHtml::link(CHtml::encode("Refresh to clean up and get just the empty payer
 <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . "/static/jquery.jeditable.mini.js")?>
 
 <table class="bordertable">
-<tr><th>Check Num</th><th>Check Date</th><th>Payer (click to edit)</th><th>Student(s)</th></tr>
+<tr><th>Check Num</th><th>Check Date</th><th>Payer (click to edit)</th><th>Student(s)</th><th></th></tr>
 <?php
 foreach($models as $model){
       echo '<tr><td class="financial">' . CHtml::encode($model->check_num) . '</td>';
       echo '<td class="financial">' . Yii::app()->format->date($model->check_date). '</td>';
-      echo '<td class="edit" id="'. $model->id  . '">' . CHtml::encode($model->payer) . '</td>';
+      echo '<td class="edit" id="check_id_'. $model->id  . '">' . CHtml::encode($model->payer) . '</td>';
       echo '<td>';
       foreach($model->incomes as $inc){
           echo $inc->student->full_name . ' ';
       }
-      echo'</td>';
-      echo '</tr>';
+      echo'</td><td>';
+      echo CHtml::link("View", 
+                 array("CheckIncome/view",
+                       'id' => $model->id
+                     ));
+      echo '</td></tr>';
 }
 
 ?>
@@ -30,16 +34,19 @@ foreach($models as $model){
 </table>
 
 <script type="text/javascript">
+//<![CDATA[
 	jQuery(function($) {
             $('.edit').editable(
                 '<?= $this->createUrl('CheckIncome/jsonUpdate') ?>', 
                 {
-                indicator : "<img src='<?= Yii::app()->baseUrl ?>/images/indicator.gif'>",
+                indicator : '<img src="<?= Yii::app()->baseUrl ?>/images/indicator.gif" alt="progress" />',
                         tooltip   : "Click to edit...",
                         name : 'CheckIncome[payer]',
                         id: 'CheckIncome[id]',
                         style  : "inherit"
                         });
         });
+
+//]]>
 
 </script>
