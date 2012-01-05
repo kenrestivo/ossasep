78ab35a733189557fd23c44ac54f0a74ae40c619
ken restivo <ken@restivo.org>
2012-01-04 23:46:32 -0800

    add form crap

--- protected/views/signup/multi_entry.php
+++ protected/views/signup/multi_entry.php
@@ -1,6 +1,11 @@
 <h3>Multi entry of classes for <?= $student->summary ?></h3>
 
 
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'multi-signup-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
 <table class="signups">
 <tr>
 <th>Student</th>
@@ -13,3 +18,17 @@
 <?php echo $this->renderPartial('_row', array('student'=>$student)); ?>
 
 </table>
+
+
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+
+<?php $this->endWidget(); ?>
+
+
+
+<?php echo $this->renderPartial('/site/_unsaved_changes_warning',
+                                array('form_id'=>"#check-income-form")); ?>
