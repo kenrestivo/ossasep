130b18299228db739a064d891ed623135af7484b
ken restivo <ken@restivo.org>
2012-01-05 12:42:40 -0800

    sync language with master

--- protected/views/signup/_row.php
+++ protected/views/signup/_row.php
@@ -93,7 +93,7 @@ echo '<div id="Signup_'. $index. '_additional_info"  >';
                                 url:'<?= CController::createUrl('ClassInfo/json') ?>',
                                 success: 
                             function(data){
-                                $('#Signup_' + id + '_additional_info').text('Student ' + data['enrolled_count'] + ' of ' + data['max_students']);
+                                $('#Signup_' + id + '_additional_info').html('signup #' + data['enrolled_count'] + '<br />(' + data['max_students'] + ' max)');
                                 if(parseInt(data['enrolled_count']) > parseInt(data['max_students'])){
                                     $('#Signup_' + id + '_status').val('Waitlist');
                                 } else{
