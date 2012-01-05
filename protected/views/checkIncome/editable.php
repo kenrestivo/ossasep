ca5c9ae15659989313543001cd77ff422a794180
ken restivo <ken@restivo.org>
2012-01-05 14:43:51 -0800

    nice, i'll call this done

--- protected/views/checkIncome/editable.php
+++ protected/views/checkIncome/editable.php
@@ -1,7 +1,7 @@
 <h3>Shows only checks with no payer (for now)</h3>
 <p>
 <?php
-echo CHtml::link(CHtml::encode("Refresh to get just the empty payers"), 
+echo CHtml::link(CHtml::encode("Refresh to clean up and get just the empty payers"), 
                  array("CheckIncome/editable",
                        ));
 ?>
@@ -10,14 +10,18 @@ echo CHtml::link(CHtml::encode("Refresh to get just the empty payers"),
 <?php Yii::app()->clientScript->registerCoreScript("jquery")?>
 <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . "/static/jquery.jeditable.mini.js")?>
 
-<table class="bordertable financial">
+<table class="bordertable">
 <tr><th>Check Num</th><th>Check Date</th><th>Payer (click to edit)</th><th>Student(s)</th></tr>
 <?php
 foreach($models as $model){
-      echo '<tr><td>' . CHtml::encode($model->check_num) . '</td>';
-      echo '<td>' . Yii::app()->format->date($model->check_date). '</td>';
+      echo '<tr><td class="financial">' . CHtml::encode($model->check_num) . '</td>';
+      echo '<td class="financial">' . Yii::app()->format->date($model->check_date). '</td>';
       echo '<td class="edit" id="'. $model->id  . '">' . CHtml::encode($model->payer) . '</td>';
-      echo '<td>'. '</td>';
+      echo '<td>';
+      foreach($model->incomes as $inc){
+          echo $inc->student->full_name . ' ';
+      }
+      echo'</td>';
       echo '</tr>';
 }
 
