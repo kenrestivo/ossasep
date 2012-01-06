725941892580d6b915e3b129f12d197442a3c083
ken restivo <ken@restivo.org>
2012-01-05 19:52:49 -0800

    well kinda

--- protected/views/admin/student_financial.php
+++ protected/views/admin/student_financial.php
@@ -1,6 +1,12 @@
 <?php
 foreach($models as $model){
     echo '<h3>' . $model->full_name . '</h3>';
+
+    $this->renderPartial(
+        '/signup/_payment_summary',
+        array('model' =>$model));
+
+
 }
 
 ?>
---
+++ protected/views/signup/_payment_summary.php
@@ -0,0 +1,20 @@
+<?php
+
+foreach($model->signups as $signup){
+    echo '<p>' . $signup->class->summary . '</p>';
+
+}
+
+?>
+
+
+ classes w/ following info:
+   enrolled/waitlist
+2
+  
+   ck #
+   ck date
+   ck total
+   $ 	 applied to class
+   remainder due
+ class note
