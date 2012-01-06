39a313cf850e2a779e7f9664cc1ec8ba10dfc6a5
ken restivo <ken@restivo.org>
2012-01-05 20:03:29 -0800

    almost works

--- protected/views/signup/_class_summary.php
+++ protected/views/signup/_class_summary.php
@@ -5,6 +5,11 @@ foreach($model->signups as $signup){
     echo '<li><span>' . $signup->class->summary . '</span>';
     echo '<span>' . $signup->status . '</span>';
     echo '<p>' . $signup->note . '</p>';
+
+    $this->renderPartial(
+        '/signup/_payment_summary',
+        array('model' =>$signup));
+
     echo '</li>';
 }
 
--- protected/views/signup/_payment_summary.php
+++ protected/views/signup/_payment_summary.php
@@ -1,8 +1,31 @@
-  
-   ck #
-   ck date
-   ck total
-   $ 	 applied to class
+
+<?php
+$this->widget('zii.widgets.grid.CGridView', array(
+                  'id'=>'income-grid',
+                  'dataProvider'=>new KArrayDataProvider(
+                      $model->income),
+                  'selectionChanged'=>
+                  ZHtml::clickableRow('CheckIncome/view', 'join'),
+                  'columns'=>array(
+                      'class.summary:text:Class',
+                      'amount:currency:Split Check Amount Assigned',
+                      'check.check_num:text:Check #',
+                      'check.payer:text:Payer',
+                      'check.amount:currency:Total Check Amount',
+                      'check.check_date:date:Check Date',
+                      'check.delivered:date:Delivered to Company',
+                      'check.deposit.deposited_date:date:Deposited',
+                      'check.returned:date:Returned to Payer',
+                      array(
+                          'class'=>'CompositeButtonColumn',
+                          'modelClassName' => 'Income',
+                          'template'=>'{update}{delete}',
+                          'returnTo' => Yii::app()->request->requestUri
+                          ),
+                      ),
+                  )); 
+
+?>
    remainder due
 
 
