1a3379a995d0503f6765d3564a8ce433ffadde77
ken restivo <ken@restivo.org>
2012-01-07 04:20:32 -0800

    well this actually works ish

--- protected/views/classInfo/_owed.php
+++ protected/views/classInfo/_owed.php
@@ -18,3 +18,50 @@ foreach($model->sortedSignups as $s){
     ?>
 
         </table>
+
+
+<?php
+
+foreach($students as $st){
+    echo '<span class="span-11">';
+
+    echo CHTML::link(CHtml::encode("Add New Check for ". $st->full_name) .  '<br />', 
+                 array("CheckIncome/multiEntry",
+                       'student_id' => $st->id,
+                       'class_id' => $model->id,
+                       'company_id' => $model->company_id,
+                       'returnTo' => Yii::app()->request->requestUri));
+
+
+
+    echo "</span><span class=\"span-9 last\">";
+    $un = CheckIncome::underAssignedChecks(null, $st->id);
+    if(count($un) > 0){
+        echo "Or pick from one of these checks:<br />";
+    }
+    foreach($un as $check){
+        echo CHTML::link($check->summary,
+                 array("Income/create",
+                       'check_id' => $check->id,
+                       'student_id' => $st->id,
+                       'class_id' => $model->id,
+                       'returnTo' => Yii::app()->request->requestUri));
+        echo CHtml::encode(': '.Yii::app()->format->currency($check->unassigned) . ' available');
+        echo '<br />';
+
+
+    }
+    echo "</span><br />";
+
+
+}
+
+?>
+
+<div class="clear"></div>
+
+<?php
+if(count($students) < 1){
+    echo "<em>All paid up! Nothing owed.</em>";
+}
+?>
