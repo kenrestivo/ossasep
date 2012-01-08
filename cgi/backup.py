edcf79529ca81136443d0a6abf32c97aa2a57c30
ken restivo <ken@restivo.org>
2012-01-08 08:21:02 -0800

    attempt

--- cgi/backup.py
+++ cgi/backup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 import cgi
+import datetime
+import os
 import cgitb; cgitb.enable()  # for troubleshooting
 
 print "Content-Type: application/octet-stream"
 print 'Content-Disposition: attachment;filename="%s.sql.bz2"' %(
-    'asep-live-backup', )
+    'asep-live-backup',
+    datetime.datetime.today().strftime('%Y%m%d%H%M%S'))
+print 'Cache-Control: max-age=0'
 print
 
+# ok, now the data
+
+os.system("mysqldump -uroot -ptesting asep| bzip2 -c")
