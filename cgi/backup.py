c8f2b0a4a97202528057fa518f02744b80018cfe
ken restivo <ken@restivo.org>
2012-01-08 08:05:13 -0800

    beginnings of this

---
+++ cgi/backup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
+
+print "Content-Type: application/octet-stream"
+print 'Content-Disposition: attachment;filename="%s.sql.bz2"' %(
+    'asep-live-backup', )
+print
+
+print """
+""" % cgi.escape(message)
