9e339a453617f0a53ed5c2fccfca322562669742
ken restivo <ken@restivo.org>
2012-01-09 22:19:11 -0800

    amazingly, works

--- protected/views/depositDetails/_payments.php
+++ protected/views/depositDetails/_payments.php
@@ -15,11 +15,16 @@ $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
                   // additional javascript options for the autocomplete plugin
                   'options'=>array(
                       'minLength'=>'2',
+/// needed so that the id doesn't populate the select box
+                      'focus'=>'js:function( event, ui ) {
+          $( "#CheckIncome[payer]" ).val( ui.item.label );
+   return false;
+  }',
                       'select'=>'js:function( event, ui ) {
 $("#labelhere").html( ui.item.label );
 $("#idhere").html( ui.item.value ); 
 return false; }',
-     ),
+                      ),
                   ));
 echo CHtml::submitButton('Add');
 echo CHtml::endForm();
