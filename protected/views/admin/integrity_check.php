0d403e1761bd755658c5218880560b8992f0f60d
ken restivo <ken@restivo.org>
2012-01-11 19:20:48 -0800

    this is awesome

--- protected/views/admin/integrity_check.php
+++ protected/views/admin/integrity_check.php
@@ -1,3 +1,5 @@
+<h3>Checks with unassigned amounts</h3>
+
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'check-income-grid',
 	'dataProvider'=>new KArrayDataProvider(
@@ -5,16 +7,16 @@
     'htmlOptions'=>array('style'=>'cursor: pointer;'),
     'selectionChanged'=>ZHtml::clickableRow('CheckIncome/view'),
 	'columns'=>array(
-		'check_num:ntext',
-        'cash:boolean',
-		'payer:ntext',
-		'amount:currency',
-		'check_date:date',
-		'payee.name:text:Payee',
+		'check_num:ntext:Check #',
+		'amount:currency:Total Check Amount',
         'unassigned:currency:Un-Assigned',
+        'cash:boolean:Cash?',
+		'payer:ntext:Payer',
+		'check_date:date:Check Date',
+		'payee.name:text:Payee',
 		'deposit.deposited_date:date:Deposited On',
-        'delivered:date',
-        'returned:date',
+        'delivered:date:Delivered to Company',
+        'returned:date:Returned to Student',
 		'session.summary:text:Session',
 		array(
 			'class'=>'CButtonColumn',
