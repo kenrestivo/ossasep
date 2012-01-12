8f2a69a4f8c763f3bcf4cb988940c2cb4328aea6
ken restivo <ken@restivo.org>
2012-01-12 00:56:09 -0800

    formatc

--- protected/views/depositDetails/printable.php
+++ protected/views/depositDetails/printable.php
@@ -1,7 +1,7 @@
-
+<div class="span-10">
 <img src="<?= Yii::app()->request->baseUrl; ?>/images/CashCheckReconciliation-img1.jpg" alt="logo" />
 
-<p><strong>CASH, COIN, and CHECK RECONCILIATION WORKSHEET</strong></p>
+<p><br /><strong>CASH, COIN, and CHECK RECONCILIATION WORKSHEET</strong></p>
 
 
 <?php $this->renderPartial("_cash_summary", 
@@ -11,15 +11,16 @@
                      array('model' => $model)); ?>
 
 
+</div>
 
 
-<p><strong>TOTAL DEPOSIT<strong>  <?= Yii::app()->format->currency($model->total_submitted)  ?></p> 
-
-
-
+<div class="span-12 last">
 <?php $this->renderPartial("_check_summary", 
                      array('model' => $model)); ?>
 
+</div>
+<div class="clear"></div>
+<p><strong>TOTAL DEPOSIT<strong>  <?= Yii::app()->format->currency($model->total_submitted)  ?></p> 
 
 
 
