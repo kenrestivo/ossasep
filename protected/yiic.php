c82aefd7e49e8d1c4f05c5e3dff63c712f286a5e
ken restivo <ken@restivo.org>
2012-01-22 01:27:57 -0800

    make yiic use proper path to framework. also try the ini_set hack (which does not appear to help)

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,11 @@
 <?php
 
+// for using yiic via a repl from emacs
+ini_set('max_execution_time', 86400); //24 hours
+ini_set('max_input_time', 86400); // 24 hours
+
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../../../../usr/local/yii-framework/yiic.php';
+$yiic='/usr/local/yii-framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
