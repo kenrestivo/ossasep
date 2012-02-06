7d938b8c63d66f45580d2f2c2f047c5e9e8906bb
ken restivo <ken@restivo.org>
2012-02-05 21:12:53 -0800

    remove old migrations

--- protected/data/proto-migrations.sql
+++
@@ -1,2 +0,0 @@
-alter table class_session add column `registration_starts` datetime DEFAULT '0000-00-00 00:00:00';
-update class_session set registration_starts = '2012-01-04 08:00:00' where id = 1;
