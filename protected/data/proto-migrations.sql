9a92d3616cb17c3a166faeef1f4782f013354af2
ken restivo <ken@restivo.org>
2012-02-05 16:22:41 -0800

    update migration and todo

--- protected/data/proto-migrations.sql
+++ protected/data/proto-migrations.sql
@@ -1,1 +1,2 @@
 alter table class_session add column `registration_starts` datetime DEFAULT '0000-00-00 00:00:00';
+update class_session set registration_starts = '2012-01-04 08:00:00' where id = 1;
