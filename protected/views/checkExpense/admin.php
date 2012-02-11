71b6381b8c377ff60ab086a5ff561f90cc7cda69
ken restivo <ken@restivo.org>
2012-02-11 15:54:31 -0800

    fix text

--- protected/views/checkExpense/admin.php
+++ protected/views/checkExpense/admin.php
@@ -5,7 +5,7 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'Create Payment', 'url'=>array('create')),
+	array('label'=>'Create Check Payables', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
