f08aaaefc23e74a46d77003777a8c2f57102d6ee
ken restivo <ken@restivo.org>
2012-02-11 17:00:21 -0800

    show session

--- protected/views/admin/signupsheet.php
+++ protected/views/admin/signupsheet.php
@@ -1,4 +1,4 @@
-<h1>Signup Sheet - Front Office</h1>
+<h1>Signup Sheet - Front Office - for <?= CHtml::encode(ClassSession::current()->summary) ?></h1>
 <?php
 foreach($classes as $class){
     $this->renderPartial(
