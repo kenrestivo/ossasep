cd1b312e35cc36620c183ccabb776dae4cc1ab06
ken restivo <ken@restivo.org>
2012-02-27 18:25:59 -0800

    split in two

--- protected/views/report/signupboxes.php
+++ protected/views/report/signupboxes.php
@@ -1,10 +1,26 @@
 <h1>Signup Form (checkboxes)</h1>
 
+<div class="span-11">
+
+
 <?php 
-foreach($classes as $class_split){
     $this->renderPartial(
         '_checkbox_table',
-        array('classes' => $class_split));
-}
-
+        array('classes' => $classes[0]));
 ?>
+
+</div>
+
+
+<div class="span-11 last">
+
+
+<?php 
+    $this->renderPartial(
+        '_checkbox_table',
+        array('classes' => $classes[1]));
+?>
+
+</div>
+
+<div class="clear"></div>
