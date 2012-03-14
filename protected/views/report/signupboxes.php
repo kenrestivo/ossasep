f0ac959b8db8d8b52848050ae7a7c63b3bd4074e
ken restivo <ken@restivo.org>
2012-03-13 22:55:25 -0700

    gonna call this done!

--- protected/views/report/signupboxes.php
+++ protected/views/report/signupboxes.php
@@ -1,11 +1,10 @@
 <?php   Yii::app()->clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/printabletable.css', 'print'); ?>
 
 <div class="span-17">
-<h1>After-School Enrichment Program – <?= CHtml::encode($this->savedSessionSummary()) ?></h1>
+<h1 class="fakespan-17 center">After-School Enrichment Program – <?= CHtml::encode($this->savedSessionSummary()) ?></h1>
 
-    <h3>Classes begin <strong><?= date('F jS', strtotime(ClassSession::current()->start_date)) ?></strong>.</h3>
-    <!-- TODO: make the reservation begin date dynamically generated -->
-    <h3>Reservations begin <strong>
+    <h3 class="fakespan-17 center">Classes begin <strong><?= date('F jS', strtotime(ClassSession::current()->start_date)) ?></strong>.<br />
+    <h3 class="fakespan-17 center">Reservations begin <strong>
 <?php
     echo date("l, F jS \a\\t g:i a", strtotime(ClassSession::current()->registration_starts));
 ?>
@@ -50,7 +49,7 @@ Return this completed form, with payment, to the Front Office</h3>
 <p>Child’s Name ___________________________________________  Grade ______</p>
 <p>Parent(s) or Guardian’s Name(s)  ____________________________ Email __________________ </p>
 <p>Daytime Contact #s (1)_________________  (2)__________________ (3) ___________________</p>
-<p><b>&gt;&gt; Please write your child’s name on the check! &lt;&lt;</b></p>
+<p class="fakespan-17 center" ><b>&gt;&gt; Please write your child’s name on the check! &lt;&lt;</b></p>
 
 
 <div class="span-8">
