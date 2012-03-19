27ef5d169b07dad34af2d48d9e3cfde9f6c86562
ken restivo <ken@restivo.org>
2012-03-19 08:23:53 -0700

    they want both, they got both

--- protected/views/report/_signup_form.php
+++ protected/views/report/_signup_form.php
@@ -1,5 +1,7 @@
 <!-- TODO: do not hard code this, put it in the database -->
 
-<?= CHtml::link(CHtml::encode("Signup Form (PDF) ") . 
+<a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Report/signupboxes">Signup Form</a>
+
+<?= CHtml::link(CHtml::encode("(PDF)") . 
                 '<img src="images/pdficon_small.gif" alt="PDF" />' , 
                 Yii::app()->baseUrl . "/static/Spring2012SignUp.pdf"); ?>
