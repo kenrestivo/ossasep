7bfe6414cc0712d8bd2efee3fa40aaae058914d5
ken restivo <ken@restivo.org>
2013-02-11 13:10:22 -0800

    migration complete, no longer needed.

--- protected/data/proto-migrations.sql
+++
@@ -1,3 +0,0 @@
-alter table `extra_fee` add column pay_to_instructor tinyint(1) default 1;
-update  extra_fee set pay_to_instructor = 0 where description like "%PTO%";
-update  extra_fee set pay_to_instructor = 1 where description not like "%PTO%";
