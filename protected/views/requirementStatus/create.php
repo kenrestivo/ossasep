08abc7453f49d93abbaeab6326a3ef24ca85242c
ken restivo <ken@restivo.org>
2013-04-14 02:19:03 -0700

    Remove more unused menus.

--- protected/views/requirementStatus/create.php
+++ protected/views/requirementStatus/create.php
@@ -1,10 +1,6 @@
 <?php
 
 
-$this->menu=array(
-	array('label'=>'List RequirementStatus', 'url'=>array('index')),
-	array('label'=>'Manage RequirementStatus', 'url'=>array('admin')),
-);
 ?>
 
 <h1>Create RequirementStatus</h1>
--- protected/views/requirementStatus/update.php
+++ protected/views/requirementStatus/update.php
@@ -5,12 +5,6 @@ $this->breadcrumbs=array(
     );
 
 
-$this->menu=array(
-	array('label'=>'List RequirementStatus', 'url'=>array('index')),
-	array('label'=>'Create RequirementStatus', 'url'=>array('create')),
-	array('label'=>'View RequirementStatus', 'url'=>array('view', 'instructor_id'=>$model->instructor_id, 'requirement_type_id' => $model->requirement_type_id)),
-	array('label'=>'Manage RequirementStatus', 'url'=>array('admin')),
-);
 ?>
 
 <h1>Update RequirementStatus <?php echo $model->instructor->full_name. " " . $model->requirement_type->description  ?></h1>
