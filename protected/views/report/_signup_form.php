4499a8bcdd3536e6abbdddaedc41691798b8fba8
ken restivo <ken@restivo.org>
2013-09-01 15:07:47 -0700

    Use the printable icon, just print the damn form.

--- protected/views/report/_signup_form.php
+++ protected/views/report/_signup_form.php
@@ -1,10 +1,14 @@
 
 <?php 
  // TODO: do not hard code this, put it in the database 
-$show_pdf = true; 
+$show_pdf = false; 
+
+$printer_image = $show_pdf ? "" :  CHtml::image(Yii::app()->request->baseUrl.'/images/printer24.png',
+												'Print') . " (Printable)";
+
 ?>
 
-<a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Report/signupboxes">Signup Form</a>
+<a href="<?php echo Yii::app()->baseUrl; ?>/index.php/Report/signupboxes">Signup Form <?= $printer_image ?></a>
 
 <?php if ($show_pdf) {
 		 echo CHtml::link(CHtml::encode("(PDF)") . 
