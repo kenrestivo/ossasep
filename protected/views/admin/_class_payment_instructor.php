b7175dde66511f86042ca73a1920d172771419b2
ken restivo <ken@restivo.org>
2013-11-30 19:13:00 -0800

    Indent

--- protected/views/admin/_class_payment_instructor.php
+++ protected/views/admin/_class_payment_instructor.php
@@ -1,17 +1,17 @@
 <?php
 
-    $owed = 0.0;
-    echo '<table class="catalog">';
-    foreach($assignments as $c){
+$owed = 0.0;
+echo '<table class="catalog">';
+foreach($assignments as $c){
     echo "<tr>";
-        $s = $c->split($c->class->paidMinusFees);
-        $owed += $s;
-        echo '<td>'. Yii::app()->format->percent($c->percentage) . ' of ';
-      echo CHtml::encode($c->class->summary) . '</td>';
-          echo '<td>'.Yii::app()->format->currency($s) . '</td>';
-        echo "</tr>";
-    }
-    echo '<td>Total Owed</td><td>' . Yii::app()->format->currency($owed).'</td>';
-    echo '</table>';
+	$s = $c->split($c->class->paidMinusFees);
+	$owed += $s;
+	echo '<td>'. Yii::app()->format->percent($c->percentage) . ' of ';
+	echo CHtml::encode($c->class->summary) . '</td>';
+	echo '<td>'.Yii::app()->format->currency($s) . '</td>';
+	echo "</tr>";
+}
+echo '<td>Total Owed</td><td>' . Yii::app()->format->currency($owed).'</td>';
+echo '</table>';
 
 ?>
